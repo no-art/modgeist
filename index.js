f0ce432b18f3a4290069f13f81227680998f7081
no-art <no.art666@gmail.com>
2018-11-20 15:27:28 +0100

    up

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 //ffmpeg -i sound1.wav -dash 1 sound1.webm
 //for file in *.*; do `ffmpeg -i "$file" -dash 1 "${file%.*}.webm"`;done
+const ortc = require('ibtrealtimesjnode').IbtRealTimeSJNode;
+var socket=new ortc();
 
+socket.setConnectionMetadata('clientConnMeta');
+socket.setClusterUrl('https://ortc-developers.realtime.co/server/ssl/2.1/')
+socket.connect('HV1kA2', 'master');
+socket.onConnected = function (o) {
+      console.log('connected')
+   }
 var fs  = require('fs');
 var formidable = require('formidable');
 var x=1;
@@ -12,6 +20,7 @@ app.use(compression());
 app.use(serveStatic(__dirname+"/www"));
 var ff=__dirname;
 app.post('/upload', function(req, res) {
+  console.log('up')
  var form = new formidable.IncomingForm();
    form.uploadDir = (__dirname+'/www/upload');
     form.on('file', function(field, file) {
@@ -19,10 +28,16 @@ app.post('/upload', function(req, res) {
             fs.rename(file.path,form.uploadDir + "/"+filename, function(err){
               if(err){
                 console.log('File could not be saved.');
-              }else{};
+              }else{
+                console.log('fs ok')
+                pubnubEmit('file',filename)
+              };
             });
         });
    form.parse(req, function(err, fields, files) {
   });
  });
 var server=app.listen(3000);
+function pubnubEmit(r,m){ 
+        socket.send('send_master',JSON.stringify({room : "master",msg :{i :r,ii :m}, uuid:"modgeist"}));
+}
